994e2022b3e8dadc3ab79d2e43eea167fb24311c
Dennis Reinkober <dennis.reinkober@digitalseals.de>
2022-03-08 02:47:02 +0100

    configured balm ui

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"
 
-const firebaseConfig = require("../firebase.config.json")
-const app = initializeApp(firebaseConfig);
+import BalmUI from 'balm-ui'; // Official Google Material Components
+import BalmUIPlus from 'balm-ui-plus'; // BalmJS Team Material Components
+import 'balm-ui-css';
 
-getAnalytics(app);
-getAuth(app)
+import firebaseConfig from "../firebase.config.json"
+const firebaseApp = initializeApp(firebaseConfig);
 
-createApp(App).mount('#app')
+getAnalytics(firebaseApp);
+getAuth(firebaseApp)
+
+const app = createApp(App)
+
+app.use(BalmUI);
+app.use(BalmUIPlus);
+
+app.mount('#app')
+
+
 
